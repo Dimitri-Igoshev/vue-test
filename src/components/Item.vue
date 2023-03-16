3469e1ee1bcbefc14a30998d0365a105ba10aedd
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-03-16 10:34:28 +0100

    Task

---
+++ src/components/Item.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="item" @click="$emit('select', item)">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    }
+  },
+  computed: {
+    name() {
+      return this.item?.name ? this.item.name : ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: .2rem solid teal;
+  color: teal;
+  padding: 1rem;
+  cursor: pointer;
+}
+</style>
